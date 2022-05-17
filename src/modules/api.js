dc80d18faf6d2507a0038ef74ea284885612de9a
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-05-17 17:32:20 +0200

    Choosing an external API

---
+++ src/modules/api.js
@@ -0,0 +1,23 @@
+const baseUrl = 'https://tasty.p.rapidapi.com/recipes/list';
+
+const options = {
+  method: 'GET',
+  params: { from: '0', size: '12', q: '' },
+  headers: {
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+    'X-RapidAPI-Key': '0ad5922801msh51e87bd70a9e20cp10fc6fjsn6f62ab061806',
+  },
+};
+
+const getRecipes = async () => {
+  let result;
+  try {
+    const res = await fetch(baseUrl, options);
+    result = await res.json();
+  } catch (err) {
+    return err;
+  }
+  return result;
+};
+
+export default getRecipes;
