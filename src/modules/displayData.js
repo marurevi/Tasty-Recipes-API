a33713cd8b70e6ee532f3efbd672d1018aaf5e20
marurevi <maryrev@hotmail.com>
2022-05-18 19:54:49 -0300

    Add likes buttons, icons and fix scroll bug

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,14 +1,14 @@
 import getRecipes from './api.js';
 
-const container = document.getElementById('container');
+const container = document.querySelector('.grid-container');
 
-const displayCards = (recipes) => {
+const displayCards = (recipes, nroLikes = 0) => {
   
   for (let i = 0; i < recipes.length; i += 1) {
     const card = document.createElement('ul');
     card.innerHTML = `<li><img src= ${recipes[i].thumbnail_url}></li>
     <li class="text1">${recipes[i].name}</li>
-    <li class="text2">id= ${recipes[i].id} <button type="button" class="likes">Likes</button></li>
+    <li class="text2"><button type="button" class="likes">.....</button><br>Likes ${nroLikes}</li>
     <button type= "button" class= "comment" id="${i}">Comment</button>`;
     card.className = 'card';
     container.append(card);
