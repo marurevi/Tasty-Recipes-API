6a56a14b9c54dc012db869305a87d9258198c2fa
marurevi <maryrev@hotmail.com>
2022-05-18 10:56:31 -0300

    Add scrollbar to container

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -7,14 +7,12 @@ const displayCards = async () => {
 
   for (let i = 0; i < recipes.length; i += 1) {
     const card = document.createElement('ul');
-    if (i < 9) {
-      card.innerHTML = `<li><img src= ${recipes[i].thumbnail_url}></li>
+    card.innerHTML = `<li><img src= ${recipes[i].thumbnail_url}></li>
     <li class="text1">${recipes[i].name}</li>
     <li class="text2">id= ${recipes[i].id} <button type="button" class="likes">Likes</button></li>
     <button type= "button" class= "comment">Comment</button>`;
-      card.className = 'card';
-      container.append(card);
-    }
+    card.className = 'card';
+    container.append(card);
   }
 };
 
